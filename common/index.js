c875516912ff41d087751c3b463d58a702a2be9f
Tej <tkahlon@labx.com>
2021-02-13 10:07:44 -0500

    fix: url for better speed

--- common/index.js
+++ common/index.js
@@ -1,3 +1,3 @@
-const URL = `https://laconicml.com/computer-science-curriculum-youtube-videos/`;
+const URL = `https://laconicml.com/computer-science-curriculum-youtube-videos/?googfc&google_norender=true`;
 
 export { URL };
