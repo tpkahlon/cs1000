5d7edd3461cdb9a32913f08623d49bb95fe5c4ea
tkahlon <tkahlon@labx.com>
2021-02-26 10:57:46 -0500

    fix: ditch cardcolumns component

--- components/Lecturers.js
+++ components/Lecturers.js
@@ -6,20 +6,26 @@ const Lecturers = () => {
   return (
     <>
       <h2 className="text-center m-0 my-3 h3 featured">Featured Lecturers</h2>
-      <CardColumns className="lecturers">
-        {DATA.sort((a,b) => a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1).map(({ id, name, logo, url }) => (
-          <Card key={id}>
-            <Card.Link href={url} target="_blank" rel="noopener noreferrer">
-              <Card.Img variant="top" src={logo} alt={`${name} logo`} />
-              <Card.Body className="p-2 mt-2 text">
-                <Card.Title className="text-capitalize text-secondary m-0 p-0">
-                  {name}
-                </Card.Title>
-              </Card.Body>
-            </Card.Link>
-          </Card>
+      <div className="lecturers">
+        {DATA.sort((a, b) =>
+          a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
+        ).map(({ id, name, logo, url }) => (
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="lecturers__card"
+            key={id}
+          >
+            <img variant="top" src={logo} alt={`${name} logo`} />
+            <div className="p-2 mt-2 text">
+              <div className="text-capitalize text-secondary m-0 p-0">
+                {name}
+              </div>
+            </div>
+          </a>
         ))}
-      </CardColumns>
+      </div>
     </>
   );
 };
