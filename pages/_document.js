a6c0db10f3bb095a58554884c0557e278c55cece
Tej <tkahlon@labx.com>
2021-05-08 09:13:13 -0400

    disable google ads for good

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,6 @@ class MyDocument extends Document {
               __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
             }}
           ></script>
-          {/* <script
-            data-ad-client="ca-pub-7162519541437651"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script> */}
           <meta name="title" content="CS1000" />
           <meta
             name="description"
