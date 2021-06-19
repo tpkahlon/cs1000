84ed529f3b9b749b31e26cf73523317071d426a3
Tej <tkahlon@labx.com>
2021-06-19 18:34:46 -0400

    fix: remove ads from this app

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,6 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script
-            data-ad-client="ca-pub-7162519541437651"
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-          ></script>
-          <script
             dangerouslySetInnerHTML={{
               __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
             }}
