7ca6a41bc97cd21acb039e26b3ec96e8e0f7e9b8
Tej <tkahlon@labx.com>
2021-09-09 13:24:39 -0400

    fix: activate ads

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,11 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7162519541437651"
+            crossorigin="anonymous"
+          ></script>
+          <script
             dangerouslySetInnerHTML={{
               __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
             }}
