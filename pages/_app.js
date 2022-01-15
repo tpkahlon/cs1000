e420b79ca037ff6c9cf5791d16a94e1b97cf1f82
Tej <tkahlon@labx.com>
2022-01-15 08:23:13 -0500

    fix: css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/index.scss";
+import "../styles/index.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
