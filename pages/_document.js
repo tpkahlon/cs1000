ebedb28ceafc74f3ddb300b88c73f8d6ec2b4d33
Tej <tkahlon@labx.com>
2022-01-15 08:06:16 -0500

    fix: add _document.js

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
+            }}
+          ></script>
+          <meta name="title" content="CS1000" />
+          <meta
+            name="description"
+            content="A reference site for a beginner, to gain insight into various subjects present around CS and Software engineering."
+          />
+          <meta
+            name="keywords"
+            content="cs1000, vercel, cs, computer, science, education, reference, material, bookmark, list, curriculum, undergraduate, degree, knowledge, resource, cs1000.vercel.app, engineer, engineering, software, web, developer, development, beginner, student, lectures"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -11,23 +11,6 @@ export default function Home({ data, token, e }) {
           rel="icon"
           href="https://image.flaticon.com/icons/png/512/3964/3964036.png"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"tpkahlon",utcoffset:"-5"}))};sessionStorage.setItem("_swa","1");`,
-          }}
-        ></script>
-        <meta name="title" content="CS1000" />
-        <meta
-          name="description"
-          content="A reference site for a beginner, to gain insight into various subjects present around CS and Software engineering."
-        />
-        <meta
-          name="keywords"
-          content="cs1000, vercel, cs, computer, science, education, reference, material, bookmark, list, curriculum, undergraduate, degree, knowledge, resource, cs1000.vercel.app, engineer, engineering, software, web, developer, development, beginner, student, lectures"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
       </Head>
       {e ? (
         <div className="min-vh-100 w-100 d-flex justify-content-center align-items-center">
